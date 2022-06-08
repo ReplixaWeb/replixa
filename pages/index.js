558a950413fa4fd44137e1c6fc2e52193651cdb6
ReplixaWeb <107123699+ReplixaWeb@users.noreply.github.com>
2022-06-08 21:08:58 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,64L48,74.7C96,85,192,107,288,138.7C384,171,480,213,576,218.7C672,224,768,192,864,165.3C960,139,1056,117,1152,117.3C1248,117,1344,139,1392,149.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-        <Header title="slebew ancrit malvi x vanli lmao yahahha hayuuu pale papale" />
-        <p className="description">
-          bilek beliau aseli 100% icikiwir shap pov shap hitam mk
+      <Header title="Hi" />
+        <p href="#" className="description">
+          LINK_GO_TO
         </p>
       </main>
 
