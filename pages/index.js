26107e32e0ab2d038bd3f6621fd5e32590ccfba6
ReplixaWeb <107123699+ReplixaWeb@users.noreply.github.com>
2022-06-08 20:12:23 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="lol xd lmao" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          lol xd lmao
         </p>
       </main>
 
